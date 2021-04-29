7e872cc7b999f1fb239d3b5eb18438872525948c
김소정 <itoggi0328@naver.com>
2021-04-29 23:47:38 +0900

    [frontend] feat: bottom 탭 변경 및  프로필 페이지 레이아웃 - bottom 탭에 프로필 탭 추가 - bottom 탭에 아이콘 추가, 이름 변경, focus 되었을 경우 색 설정 - 프로필 페이지 레이아웃 완료

--- client/app/navigations/root.js
+++ client/app/navigations/root.js
@@ -16,6 +16,7 @@ import AuthStack from './AuthStack';
 import DiaryStacks from './DiaryStack';
 import HomeStacks from './HomeStack';
 import RecommendationStacks from './RecommendationStack';
+import ProfileStacks from './ProfileStack';
 
 // style
 import theme from '../assets/theme/index';
@@ -31,10 +32,53 @@ const Tab = createBottomTabNavigator();
 
 function Tabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStacks} />
-      <Tab.Screen name="Diary" component={DiaryStacks} />
-      <Tab.Screen name="Recommendation" component={RecommendationStacks} />
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused}) => {
+          if (route.name === '홈') {
+            return (
+              <Icon
+                type="Ionicons"
+                name={focused ? 'home-sharp' : 'home-outline'}
+                style={{color: focused ? '#8AD169' : '#808285', fontSize: 25}}
+              />
+            );
+          } else if (route.name === '다이어리') {
+            return (
+              <Icon
+                type="Ionicons"
+                name="calendar-sharp"
+                style={{color: focused ? '#8AD169' : '#808285', fontSize: 25}}
+              />
+            );
+          } else if (route.name === '추천') {
+            return (
+              <Icon
+                type="MaterialCommunityIcons"
+                name={focused ? 'thumb-up' : 'thumb-up-outline'}
+                style={{color: focused ? '#8AD169' : '#808285', fontSize: 25}}
+              />
+            );
+          } else if (route.name === '프로필') {
+            return (
+              <Icon
+                type="MaterialCommunityIcons"
+                name={focused ? 'account' : 'account-outline'}
+                style={{color: focused ? '#8AD169' : '#808285', fontSize: 30}}
+              />
+            );
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#8AD169', // 탭 클릭했을 경우 색깔
+        inactiveTintColor: '#808285', // 탭 클릭하지 않았을 경우 색깔
+        style: {height: 55}, // bottom tab 높이
+      }}>
+      <Tab.Screen name="홈" component={HomeStacks} />
+      <Tab.Screen name="다이어리" component={DiaryStacks} />
+      <Tab.Screen name="추천" component={RecommendationStacks} />
+      <Tab.Screen name="프로필" component={ProfileStacks} />
     </Tab.Navigator>
   );
 }
