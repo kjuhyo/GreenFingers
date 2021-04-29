ca5d8a0d56195dca1af19b383ae67ee4da135dc7
김소정 <itoggi0328@naver.com>
2021-04-30 00:12:09 +0900

    [frontend] style: axios 관련 파일에 주석 추가

--- client/app/api/index.js
+++ client/app/api/index.js
@@ -9,7 +9,7 @@ export const instance = axios.create({
 });
 
 
-// interceptors
+// interceptors(임시. 백엔드와 협의 필요)
 instance.interceptors.response.use(function (response) {
   // Any status code that lie within the range of 2xx cause this function to trigger
   // Do something with response data
