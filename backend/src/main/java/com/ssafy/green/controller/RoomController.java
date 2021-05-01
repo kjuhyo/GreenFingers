438416e89f19864edd71e4d115d6ad0ba3e35848
gisung2215 <wjv2215@naver.com>
2021-05-01 16:43:25 +0900

    [backend] feat : rooctroller 수정

--- backend/src/main/java/com/ssafy/green/controller/RoomController.java
+++ backend/src/main/java/com/ssafy/green/controller/RoomController.java
@@ -1,5 +1,9 @@
 package com.ssafy.green.controller;
 
+import com.google.firebase.auth.AuthErrorCode;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 import com.ssafy.green.model.dto.RoomResponse;
 import com.ssafy.green.service.RoomService;
 import io.swagger.annotations.ApiOperation;
@@ -24,26 +28,42 @@ public class RoomController {
     /**
      * 방 생성
      */
-    @ApiOperation(value = "방 생성", notes="Parameter\n" +
-            "- token(RequestHeader): 액세스 토큰\n" +
+    @ApiOperation(value = "방 생성", notes = "Parameter\n" +
+            "- token(RequestHeader) : Firebase token\n" +
             "- roomName: 생성할 방 이름\n\n" +
             "Response\n" +
             "- true: 방 생성 성공\n" +
             "- false: 방 생성 실패!\n")
     @PostMapping("/create")
-    public ResponseEntity<Map<String, Object>> create(@RequestHeader("TOKEN") String token, @RequestBody CreateRoomRequest request){
-        Map<String,Object> resultMap = new HashMap<>();
-        boolean result = roomService.createRoom(token, request.getUserId(), request.getRoomName());
-        resultMap.put("response", result);
-        if(result){
-            return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+    public ResponseEntity<Map<String, Object>> create(@RequestHeader("TOKEN") String token,
+                                                      @RequestBody CreateRoomRequest request) {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        try {
+            // 1. Firebase Token decoding
+            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+            // 2. 방 생성!
+            boolean result = roomService.createRoom(decodedToken.getUid(), request.getRoomName());
+            if (result) {
+                resultMap.put("error", 0);
+                return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+            } else {
+                resultMap.put("error", 1);
+                resultMap.put("msg", "이미 존재하는 방 입니다.");
+            }
+        } catch (FirebaseAuthException e) {
+            resultMap.put("error", 1);
+            AuthErrorCode authErrorCode = e.getAuthErrorCode();
+            // 3. Token 만료 체크
+            if (authErrorCode == AuthErrorCode.EXPIRED_ID_TOKEN) {
+                resultMap.put("msg", "EXPIRED_ID_TOKEN");
+            }
         }
         return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.BAD_REQUEST);
     }
 
     @Data
-    static class CreateRoomRequest{
-        private String userId;
+    static class CreateRoomRequest {
         private String roomName;
     }
 
@@ -51,49 +71,70 @@ public class RoomController {
      * 방 조회
      */
     @ApiOperation(value = "모든 방 조회(그 안 식물까지)", notes = "사용자 로그인 토큰 필요")
-    @GetMapping("/find/{userId}")
-    public ResponseEntity<Map<String, Object>> find(
-            @RequestHeader("TOKEN") String token,
-            @PathVariable String userId
-    ){
+    @GetMapping("/find")
+    public ResponseEntity<Map<String, Object>> find(@RequestHeader("TOKEN") String token) {
+        Map<String, Object> resultMap = new HashMap<>();
 
-        Map<String,Object> resultMap = new HashMap<>();
-        List<RoomResponse> allRooms = roomService.findRooms(token, userId);
-        resultMap.put("response", allRooms);
-
-        return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+        try {
+            // 1. Firebase Token decoding
+            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+            // 2. 방 생성!
+            List<RoomResponse> allRooms = roomService.findRooms(decodedToken.getUid());
+            resultMap.put("response", allRooms);
+            resultMap.put("error", 0);
+            return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+        } catch (FirebaseAuthException e) {
+            resultMap.put("error", 1);
+            AuthErrorCode authErrorCode = e.getAuthErrorCode();
+            // 3. Token 만료 체크
+            if (authErrorCode == AuthErrorCode.EXPIRED_ID_TOKEN) {
+                resultMap.put("msg", "EXPIRED_ID_TOKEN");
+            }
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.BAD_REQUEST);
     }
 
     @Data
-    static class FineRoomRequest{
+    static class FineRoomRequest {
         private String userId;
     }
 
     /**
      * 방 삭제
      */
-    @ApiOperation(value = "방 삭제", notes="Parameter\n" +
-            "- token(RequestHeader): 액세스 토큰\n" +
-            "- {id}(PathVariable) : 삭제할 방 이름\n\n" +
+    @ApiOperation(value = "방 삭제", notes = "Parameter\n" +
+            "- token(RequestHeader) : Firebase token\n" +
+            "- {id}(PathVariable) : 삭제할 방 번호\n\n" +
             "Response\n" +
             "- true: 방 삭제 성공\n" +
             "- false: 방 삭제 실패!\n")
     @PutMapping("/delete/{id}")
     public ResponseEntity<Map<String, Object>> delete(@RequestHeader("TOKEN") String token,
-                                                      @PathVariable Long id,
-                                                      @RequestBody DeleteRoomRequest request
-    ){
-        Map<String,Object> resultMap = new HashMap<>();
-        boolean result = roomService.deleteRoom(token, id, request.getUserId());
-        resultMap.put("response", result);
-        if(result){
-            return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+                                                      @PathVariable Long id) {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        try {
+            // 1. Firebase Token decoding
+            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+            // 2. 방 삭제!
+            boolean result = roomService.deleteRoom(decodedToken.getUid(), id);
+            if (result) {
+                resultMap.put("error", 0);
+                return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+            } else {
+                // 2-1. 내 방이 아닌경우,
+                // 2-2. 해당 이름의 방이 존재하지 않는 경우,
+                resultMap.put("error", 1);
+                resultMap.put("msg", "방을 확인해 주세요.");
+            }
+        } catch (FirebaseAuthException e) {
+            resultMap.put("error", 1);
+            AuthErrorCode authErrorCode = e.getAuthErrorCode();
+            // 3. Token 만료 체크
+            if (authErrorCode == AuthErrorCode.EXPIRED_ID_TOKEN) {
+                resultMap.put("msg", "EXPIRED_ID_TOKEN");
+            }
         }
         return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.BAD_REQUEST);
-    }
-
-    @Data
-    static class DeleteRoomRequest{
-        private String userId;
     }
 }
