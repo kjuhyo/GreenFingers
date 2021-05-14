d6554aefe6821d1b07980930acfc9acf3a73b56d
qqyurr <byyulli16@gmail.com>
2021-05-14 16:55:00 +0900

    [frontend] ar 화면 띄우기
    Former-commit-id: 7632cdaad2a9c7168b72a1654ab36548aaa61bff

--- client/app/screens/ar/ArIntro.js
+++ client/app/screens/ar/ArIntro.js
@@ -1,9 +1,66 @@
 'use strict';
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {Container, Header, Content} from 'native-base';
-import {ViroARScene, ViroText} from '@viro-community/react-viro';
+import {
+  ViroARScene,
+  ViroText,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARSceneNavigator,
+  Viro3DObject,
+} from '@viro-community/react-viro';
 
 export function ArIntroScreen({navigation}) {
-  return <ViroARScene></ViroARScene>;
+  return <ViroARSceneNavigator initialScene={{scene: ArIntro}} />;
 }
+function ArIntro({navigation}) {
+  return (
+    // <View>
+    //   <Text>AR</Text>
+    // </View>
+
+    <ViroARScene>
+      <ViroAmbientLight color={'#aaaaaa'} />
+      <ViroSpotLight
+        innerAngle={5}
+        outerAngle={90}
+        direction={[0, -1, -0.2]}
+        position={[0, 3, 1]}
+        color="yellow"
+        castsShadow={true}
+      />
+      <ViroText
+        text="Hello World"
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
+      {/* <Viro3DObject
+        source={require('../../../app/cactus_gasteria.obj')}
+        resources={[
+          require('../../../app/ceramic_struktur_DIF_1k.jpg'),
+          require('../../../app/cactus_gasteria_DIF_1k.jpg'),
+          require('../../../app/cactus_ceramic_blue_DIF_1k.jpg'),
+        ]}
+        position={[-0.0, -5.5, -1.15]}
+        type="OBJ"
+      /> */}
+      {/* <ViroBox
+        position={[0, -0.5, -1]}
+        scale={[0.3, 0.3, 0.1]}
+        materials={['grid']}
+      /> */}
+    </ViroARScene>
+  );
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: 'red',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
