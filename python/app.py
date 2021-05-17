810e12dba6bf11eaf6e1f03c20c3b36a4eab334f
김부희 <kimbuhui@naver.com>
2021-05-17 14:44:59 +0900

    [backend] modify: AI update

--- python/app.py
+++ python/app.py
@@ -1,85 +1,156 @@
-from flask import Flask, render_template, request, redirect, send_file
-from PIL import Image
-
+from flask import Flask, jsonify, render_template, request, redirect, send_file
 import io
-import json
+
 import os
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-
-import torchvision
-from torchvision import datasets, models, transforms
-
 import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+import json
 import time
-import os
+from PIL import Image
+import argparse
 
 app = Flask(__name__)
 
-device = torch.device("cuda:0" if torch.cuda.is_available()
-                      else "cpu")  # device 객체
+DEFAULT_DATA_DIRECTORY = 'custom_set'
+DEFAULT_CHECKPOINT_FILENAME = './checkpoint2.pth'
+DEFAULT_TOP_K = 3
+DEFAULT_GPU = True
+FILENAME_JSON_CATEGORY = 'cat_to_name.json'
+FILEPATH_JSON_CATEGORY = DEFAULT_DATA_DIRECTORY + '/' + FILENAME_JSON_CATEGORY
+SIZE_CROP = 224
+SIZE_RESIZE = 256
+NORMALIZE_MEAN = [0.485, 0.456, 0.406]
+NORMALIZE_STD = [0.229, 0.224, 0.225]
 
-transforms_train = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.RandomHorizontalFlip(),  # 데이터 증진(augmentation)
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [
-                         0.229, 0.224, 0.225])  # 정규화(normalization)
-])
 
-transforms_test = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+def parse_input_arguments():
+    parser = argparse.ArgumentParser(description = "Predict using a deep neural network")
+    parser.add_argument('--image_path', type = str, default ="", help = 'Dataset path')
+    parser.add_argument('--checkpoint_path', type = str, default = DEFAULT_CHECKPOINT_FILENAME, help = 'Path to load trained model checkpoint')
+    parser.add_argument('--top_k', type = int, default = DEFAULT_TOP_K, help = 'Top K most likely classes')
+    parser.add_argument('--category_names', type = str, default = FILEPATH_JSON_CATEGORY, help = 'File .json for the mapping of categories to real names')
+    parser.add_argument('--gpu', action = "store_true", default = DEFAULT_GPU, help = 'Use GPU if available')
 
-data_dir = './custom_dataset'
-train_datasets = datasets.ImageFolder(
-    os.path.join(data_dir, 'train'), transforms_train)
-test_datasets = datasets.ImageFolder(
-    os.path.join(data_dir, 'test'), transforms_test)
+    args = parser.parse_args()
 
-class_names = train_datasets.classes
+    return args.image_path, args.checkpoint_path, args.top_k, args.category_names, args.gpu
 
+
+def load_model_checkpoint(file_path):
+    checkpoint = torch.load(file_path)
+    learning_rate = checkpoint['learning_rate']
+    model = getattr(torchvision.models, checkpoint['network'])(pretrained = True)
+    model.classifier = checkpoint['classifier']
+    model.epochs = checkpoint['epochs']
+    model.optimizer = checkpoint['optimizer']
+    model.load_state_dict(checkpoint['state_dict'])
+    model.class_to_idx = checkpoint['class_to_idx']
+    
+    return model
+
+
+def process_image(pil_image):
+    img_loader = transforms.Compose([transforms.Resize(SIZE_RESIZE),
+                                    transforms.CenterCrop(SIZE_CROP), 
+                                    transforms.ToTensor()])
+    
+    pil_image = img_loader(pil_image).float()
+    
+    np_image = np.array(pil_image)    
+    
+    mean = np.array(NORMALIZE_MEAN)
+    std = np.array(NORMALIZE_STD)
+    np_image = (np.transpose(np_image, (1, 2, 0)) - mean) / std    
+    np_image = np.transpose(np_image, (2, 0, 1))
+            
+    return np_image
+
+
+def get_prediction(image_path, model, top_k_probabilities = DEFAULT_TOP_K):
+    # Use GPU if it's available
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model.to(device)
+    model.eval()
+
+    pil_image = Image.open(image_path)
+    
+    np_image = process_image(pil_image)
+    tensor_image = torch.from_numpy(np_image)
+    
+    inputs = Variable(tensor_image)
+    
+    if torch.cuda.is_available():
+        inputs = Variable(tensor_image.float().cuda())           
+        
+    inputs = inputs.unsqueeze(dim = 0).float()
+    log_probabilities = model.forward(inputs)
+    probabilities = torch.exp(log_probabilities)    
+
+    top_probabilities, top_classes = probabilities.topk(top_k_probabilities, dim = 1)
+    class_to_idx_inverted = {model.class_to_idx[c]: c for c in model.class_to_idx}
+    top_mapped_classes = list()
+    
+    for label in top_classes.cpu().detach().numpy()[0]:
+        top_mapped_classes.append(class_to_idx_inverted[label])
+    
+    return top_probabilities.cpu().detach().numpy()[0], top_mapped_classes
+
+
+def get_mapping_label_name_categories(category_names):
+    print('\t' + category_names)
+    with open(category_names, 'r') as f:
+        category_label_to_name = json.load(f)
+    return category_label_to_name
+
+
+def load_model(category_names, checkpoint_path):
+    print('Load the model checkpoint from {}'.format(checkpoint_path))
+    model = load_model_checkpoint(checkpoint_path)
+
+    print('Load category name and label mapping')
+    category_label_to_name = get_mapping_label_name_categories(category_names)
+
+    return model, category_label_to_name
+
+
+def predict(image_path, checkpoint_path, top_k, category_names, gpu):
+    model, category_label_to_name = load_model(category_names, checkpoint_path)
+
+    top_probabilities, top_classes = get_prediction(image_path, model, top_k_probabilities = top_k)
+
+    print('Probabilities: ', top_probabilities)
+    print('Categories:    ', top_classes)
+    print('Result:    ', top_classes[0])
+    return top_classes[0]
+
+######################################################################################################
 
 @app.route("/")
 def main():
     return render_template("upload.html")
 
-# 이미지를 읽어 결과를 반환하는 함수
-
-
-def get_prediction(image_bytes):
-    # state_dict만 저장
-    model = models.resnet34(pretrained=True)
-    num_features = model.fc.in_features
-    model.fc = nn.Linear(num_features, 208)
-    model.load_state_dict(torch.load('weight_only1.pth', map_location=device))
-    # 모델 전체 저장
-    # model = torch.load('./entire_model1.pth',map_location=device)
-    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
-    image = transforms_test(image).unsqueeze(0).to(device)
-
-    with torch.no_grad():
-        outputs = model(image)
-        _, preds = torch.max(outputs, 1)
-    return class_names[preds[0]]
-
-
 @app.route('/predict', methods=['POST'])
-def predict():
-    # 이미지 바이트 데이터 받아오기
+def result_predict():
+    image_path, checkpoint_path, top_k, category_names, gpu = parse_input_arguments()
+    
+    # 이미지 받아오기
     f = request.files['file']
-    image_bytes = f.read()
 
     # 분류 결과 확인 및 클라이언트에게 결과 반환
-    class_name = get_prediction(image_bytes=image_bytes)
+    class_name = predict(f, checkpoint_path, top_k, category_names, gpu)
     print("결과:", {'class_name': class_name})
-    return render_template("result.html", name=f.filename, result=class_name)
+    response = {'result' : class_name}
+    return render_template("result.html", name=f.filename, result=class_name) # 로컬 테스트용
+    # return jsonify(response) # 배포용
 
-
-# 웹 서버 생성
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0',port=4000)
